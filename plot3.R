7eee20d70d4950414e2c1cb3a19ab88cb6e58d1c
Jackie Yang <yangjiekun@gmail.com>
2014-06-08 14:10:54 -0400

    Add plot3.R and plot3.png.

--- plot3.R
+++ plot3.R
@@ -10,6 +10,9 @@ powerData$Time <- strptime(powerData$Time, format = "%Y-%m-%d, %H:%M:%S")
 
 power2Day <- powerData[powerData$Date == as.Date(c("2007-02-01")) | powerData$Date == as.Date(c("2007-02-02")),]
 
-png("plot1.png")
-plot(power2Day$Time, power2Day$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab = "", type = "l")
+png("plot3.png")
+with(power2Day, plot(Time, Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l"))
+with(power2Day, lines(Time, Sub_metering_2, col = "red"))
+with(power2Day, lines(Time, Sub_metering_3, col = "blue"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
